c7ee38544243249e287fa5bdf5950855fb0f29e6
Harry Soar <hs263858@falmouth.ac.uk>
2022-05-16 16:24:07 +0100

    Added Base GUI and HexToColor function

--- Editor/ArduinoManagerEditor.cs
+++ Editor/ArduinoManagerEditor.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
 using UnityEditor;
 
+[CustomEditor(typeof(ArduinoManager))]
 public class ArduinoManagerEditor : Editor
 {
-    public ArduinoManager arduinoManager;
 
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
+    }
+
+    private Color HexToColour(string hex)
+    {
+        Color color = new Color();
+        ColorUtility.TryParseHtmlString(hex, out color);
+        return color;
     }
 }
