bb8f997f748c09a6e5b21ae581b16d447760b634
Harry Soar <hs263858@falmouth.ac.uk>
2022-07-19 15:24:42 +0100

    Modified Device class to split hardware info into VID and PID on construction.

--- Runtime/Device.cs
+++ Runtime/Device.cs
@@ -1,17 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
-
+[System.Serializable]
 public class Device
 {
+    public string Port;
+    public string FriendlyName;
+    public string VID;
+    public string PID;
+    public bool Chosen = false;
     public Device(string port, string friendlyName, string[] hwInfo)
     {
         Port = port;
         FriendlyName = friendlyName;
-        HWInfo = hwInfo;
+
+        // HWINFO comes through like this.
+        // Take the first line and split it, then assign VID and PID correctly
+        // USB\VID_16C0&PID_0483&REV_0277
+        // USB\VID_16C0 & PID_0483
+
+        char[] delimiters = { '\\', '&', '_' };
+        string[] splitInfo = hwInfo[0].Split(delimiters);
+        VID = splitInfo[2];
+        PID = splitInfo[4];
+    }
+    public Device(string port, string friendlyName, string vID, string pID)
+    {
+        Port = port;
+        FriendlyName = friendlyName;
+        VID = vID;
+        PID = pID;
     }
 
-    public string Port { get;}
-    public string FriendlyName { get;}
-    public string[] HWInfo { get;}
 }
